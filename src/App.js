3d4231d9c6881321ba6ef9256af42fcad6f36079
Sansh1305 <sanshita.goyal2022@vitstudent.ac.in>
2024-09-03 23:26:21 +0530

    Please work

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world my name is sanshita are u fine!
+       trial please work i am tired super duper.
     </h1>
   );
 }
